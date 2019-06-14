4a15d141434956fd396742de05c47463c10e55f6
mtkachenko <mtkachenko@magecom.us>
2019-06-14 19:50:46 +0300

    Initial commit

---
+++ server/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema')
+
+const app = express();
+const PORT = 4000;
+
+app.use('/graphql', graphqlHTTP({
+  schema
+}));
+
+app.listen(PORT, () => {
+  console.log('now listening for requests on port ', PORT);
+});
