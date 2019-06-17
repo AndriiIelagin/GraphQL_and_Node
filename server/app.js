9646d575df617546b35425233ea3b9499673c446
AndrewElagin <AndrewElagin@users.noreply.github.com>
2019-06-17 17:04:26 +0300

    Connect to mlab

--- server/app.js
+++ server/app.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-const schema = require('./schema/schema')
+const schema = require('./schema/schema');
+const mongoose = require('mongoose')
+
+// connect to mlab database
+mongoose.connect('mongodb://admin:test123@ds239157.mlab.com:39157/gql-test-project');
+mongoose.connection.once('open', () => {
+  console.log('connected to database');
+});
 
 const app = express();
 const PORT = 4000;
