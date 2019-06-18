b7601b1703a9575ac6dde52fc971ca53b586d3d5
mtkachenko <mtkachenko@magecom.us>
2019-06-18 21:05:03 +0300

    Add BookList component

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+
+// components
+import BookList from './components/BookList';
+
 function App() {
   return (
     <div id="main">
       <h1>Awesome Reading List</h1>
+      <BookList />
     </div>
   );
 }
